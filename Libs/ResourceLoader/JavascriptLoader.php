eb0d42e284501f3a3879ae7e9ec0d875ed37ba07
Peter Pfeufer <p.pfeufer@thamm-it.de>
2018-08-24 13:56:28 +0200

    Bootstrap Gallery not imnplemented yet ...

--- Libs/ResourceLoader/JavascriptLoader.php
+++ Libs/ResourceLoader/JavascriptLoader.php
@@ -10,7 +10,7 @@ class JavascriptLoader implements \WordPress\Plugin\BootstrapShortcodes\Libs\Int
 	public function enqueue() {
 		\wp_enqueue_script('bootstrap-js', \WordPress\Plugin\BootstrapShortcodes\Libs\Helper\PluginHelper::getInstance()->getPluginUri('bootstrap/js/bootstrap.min.js'), array('jquery'), '', true);
 		\wp_enqueue_script('bootstrap-toolkit-js', \WordPress\Plugin\BootstrapShortcodes\Libs\Helper\PluginHelper::getInstance()->getPluginUri('bootstrap/bootstrap-toolkit/bootstrap-toolkit.min.js'), array('jquery', 'bootstrap-js'), '', true);
-		\wp_enqueue_script('bootstrap-gallery-js', \WordPress\Plugin\BootstrapShortcodes\Libs\Helper\PluginHelper::getInstance()->getPluginUri('js/jquery.bootstrap-gallery.min.js'), array('jquery', 'bootstrap-js'), '', true);
+//		\wp_enqueue_script('bootstrap-gallery-js', \WordPress\Plugin\BootstrapShortcodes\Libs\Helper\PluginHelper::getInstance()->getPluginUri('js/jquery.bootstrap-gallery.min.js'), array('jquery', 'bootstrap-js'), '', true);
 		\wp_enqueue_script('bootstrap-shortcodes-for-wordpress-js', \WordPress\Plugin\BootstrapShortcodes\Libs\Helper\PluginHelper::getInstance()->getPluginUri('js/bootstrap-3-shortcodes-for-wordpress.min.js'), array('jquery'), '', true);
 	}
 }
